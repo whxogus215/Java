126c3b925aa0d4a7c3c6c45efe323f9226b60cad
whxogus215 <70999462+whxogus215@users.noreply.github.com>
2021-07-28 06:34:19 +0900

    Update PlayerLevel.java

--- Chapter3/ch16/gamelevel/PlayerLevel.java
+++ Chapter3/ch16/gamelevel/PlayerLevel.java
@@ -7,8 +7,8 @@ public abstract class PlayerLevel {
 	public abstract void turn();
 	public abstract void showLevelMessage();
 		
-
-	public void go(int count) {
+	// 
+	final public void go(int count) {
 		
 		run();
 		for(int i =0; i <count; i++) {
