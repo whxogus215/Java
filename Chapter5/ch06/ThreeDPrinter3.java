80ee3b887405500e6038ee338fcd651c21d91ff1
whxogus215 <70999462+whxogus215@users.noreply.github.com>
2021-08-04 20:50:51 +0900

    2021-08-04
    Generic 프로그래밍 구현

---
+++ Chapter5/ch06/ThreeDPrinter3.java
@@ -0,0 +1,17 @@
+package ch06;
+
+public class ThreeDPrinter3 {
+	
+	// Object는 최상위 클래스이므로 모든 자료형으로 변환이 가능!
+	private Object material;
+
+	public Object getMaterial() {
+		return material;
+	}
+
+	public void setMaterial(Object material) {
+		this.material = material;
+	}
+	
+	
+}
